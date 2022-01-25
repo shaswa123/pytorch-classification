66d1a6858e349bb2055d658772cbc0e19d523431
Shaswat <shaswat178@gmail.com>
2022-01-25 19:36:58 +0530

    Adding setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# Download the data
+pip install kaggle
+
+export KAGGLE_USERNAME=
+export KAGGLE_KEY=
+
+kaggle competitions download -c diabetic-retinopathy-detection
+
+unzip diabetic-retinopathy-detection.zip
+
+cat train.zip.* >train.zip
+unzip train.zip
+
+cat test.zip.* >test.zip
+unzip test.zip
+
+unzip trainLabels.csv.zip
+
+rm -rf *.zip
